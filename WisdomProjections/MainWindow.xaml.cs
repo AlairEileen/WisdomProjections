761ab18ea4b1dbb2713c3d5b4882bf9214ba1657
alair <dwalzixe@163.com>
2019-06-12 17:53:03 +0800

    添加emgucv sdk 添加测试ps魔棒功能，有bug，已注释

--- WisdomProjections/MainWindow.xaml.cs
+++ WisdomProjections/MainWindow.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -89,8 +93,8 @@ namespace WisdomProjections
         {
             for (var i = 0; i < MaxPersons * MaxJoints; i++)
             {
-                var joint = new Ellipse();
-                joint.Fill = Brushes.Red;
+                var joint = new System.Windows.Shapes.Ellipse();
+                joint.Fill = System.Windows.Media.Brushes.Red;
                 joint.Width = 16.0;
                 joint.Height = 16.0;
                 joint.Visibility = System.Windows.Visibility.Collapsed;
@@ -101,7 +105,7 @@ namespace WisdomProjections
             for (var i = 0; i < MaxPersons * MaxBones; i++)
             {
                 var bone = new Line();
-                bone.Stroke = Brushes.Blue;
+                bone.Stroke = System.Windows.Media.Brushes.Blue;
                 bone.StrokeThickness = 6.0;
                 bone.Visibility = System.Windows.Visibility.Collapsed;
 
@@ -159,7 +163,7 @@ namespace WisdomProjections
                 {
                     if (null == this.cbitmap || this.cbitmap.PixelWidth != this.cframe.Width || this.cbitmap.PixelHeight != this.cframe.Height)
                     {
-                        this.cbitmap = new WriteableBitmap(this.cframe.Width, this.cframe.Height, 96.0, 96.0, PixelFormats.Bgra32, null);
+                        this.cbitmap = new WriteableBitmap(this.cframe.Width, this.cframe.Height, 96, 96, PixelFormats.Bgra32, null);
                         this.cimage.Source = this.cbitmap;
                     }
 
@@ -185,14 +189,15 @@ namespace WisdomProjections
 
 
             ///create by alair end
-            
+            //TestPointCloud(sensor,depthFrame,);
+
             ///create by alair end
 
             lock (this.dpixels)
             {
                 this.dw = depthFrame.Width;
                 this.dh = depthFrame.Height;
-
+                //Console.Write("深度返回的点");
                 for (var i = 0; i < depthFrame.Pixels.Length; i++)
                 {
                     var d = depthFrame.Pixels[i];
@@ -208,7 +213,9 @@ namespace WisdomProjections
                     {
                         this.dpixels[i] = this.palette[d];
                     }
+                    //Console.Write(d+",");
                 }
+                //Console.WriteLine();
 
                 Dispatcher.BeginInvoke(new Action(() =>
                 {
@@ -216,11 +223,12 @@ namespace WisdomProjections
                     {
                         if (null == this.dbitmap || this.dbitmap.PixelHeight != this.dw || this.dbitmap.PixelHeight != this.dh)
                         {
-                            this.dbitmap = new WriteableBitmap(this.dw, this.dh, 96.0, 96.0, PixelFormats.Bgra32, null);
+                            this.dbitmap = new WriteableBitmap(this.dw, this.dh, 96, 96, PixelFormats.Bgra32, null);
                             this.dimage.Source = this.dbitmap;
                         }
 
                         this.dbitmap.WritePixels(new Int32Rect(0, 0, this.dw, this.dh), this.dpixels, this.dw * 4, 0);
+                        //this.dbitmap.WritePixels(new Int32Rect(0, 0, this.dw, this.dh), this.dpixels, this.dbitmap.BackBufferStride, 0);
                     }
                 }), DispatcherPriority.Render);
             }
@@ -242,6 +250,11 @@ namespace WisdomProjections
                     this.DrawSkeletons(this.pubData);
                 }
             }), DispatcherPriority.Render);
+        }
+
+        private void TestPointCloud(Sensor sensor)
+        {
+           //sensor.ConvertToPointCloud()
         }
 
         private void DrawSkeletons(PublishData pub)
@@ -303,7 +316,7 @@ namespace WisdomProjections
             }
 
             var p = this.Project(position, scaleX, scaleY);
-            var e = (Ellipse)this.canvas.Children[index++];
+            var e = (System.Windows.Shapes.Ellipse)this.canvas.Children[index++];
 
             Canvas.SetLeft(e, p.X - e.Width * 0.5);
             Canvas.SetTop(e, p.Y - e.Width * 0.5);
@@ -335,12 +348,12 @@ namespace WisdomProjections
             l.Visibility = System.Windows.Visibility.Visible;
         }
 
-        private Point Project(Float4 position, float scaleX, float scaleY)
+        private System.Windows.Point Project(Float4 position, float scaleX, float scaleY)
         {
             Float2 point2D = new Float2();
             this.sensor.DepthSpacePointToScreen(position, ref point2D);
 
-            var p = new Point();
+            var p =new System.Windows.Point();
             p.X = point2D.X * scaleX;
             p.Y = point2D.Y * scaleY;
 
@@ -358,12 +371,169 @@ namespace WisdomProjections
         private WriteableBitmap dbitmap;
 
         private PublishData pubData;
-
+        private IOutputArray contours;
         private const int MaxPersons = 6;
         private const int MaxJoints = 17;
         private const int MaxBones = 14;
 
+        ///create by alair
+        ///
+        public Bitmap FloodFill(Bitmap src, System.Drawing.Point location, System.Drawing.Color fillColor, int threshould)
+        {
+            try
+            {
+                Bitmap srcbmp = src;
+                Bitmap dstbmp = new Bitmap(src.Width, src.Height);
+                int w = srcbmp.Width;
+                int h = srcbmp.Height;
+                Stack<System.Drawing.Point> fillPoints = new Stack<System.Drawing.Point>(w * h);
+                System.Drawing.Imaging.BitmapData bmpData = srcbmp.LockBits(new System.Drawing.Rectangle(0, 0, srcbmp.Width, srcbmp.Height), System.Drawing.Imaging.ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+                System.Drawing.Imaging.BitmapData dstbmpData = dstbmp.LockBits(new System.Drawing.Rectangle(0, 0, dstbmp.Width, dstbmp.Height), System.Drawing.Imaging.ImageLockMode.ReadWrite, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
 
+                IntPtr ptr = bmpData.Scan0;
+                int stride = bmpData.Stride;
+                int bytes = bmpData.Stride * srcbmp.Height;
+                byte[] grayValues = new byte[bytes];
+                System.Runtime.InteropServices.Marshal.Copy(ptr, grayValues, 0, bytes);
+                System.Drawing.Color backColor = System.Drawing.Color.FromArgb(grayValues[location.X * 3 + 2 + location.Y * stride], grayValues[location.X * 3 + 1 + location.Y * stride], grayValues[location.X * 3 + location.Y * stride]);
+
+                IntPtr dstptr = dstbmpData.Scan0;
+                byte[] temp = new byte[bytes];
+                System.Runtime.InteropServices.Marshal.Copy(dstptr, temp, 0, bytes);
+
+                int gray = (int)((backColor.R + backColor.G + backColor.B) / 3);
+                if (location.X < 0 || location.X >= w || location.Y < 0 || location.Y >= h) return null;
+                fillPoints.Push(new System.Drawing.Point(location.X, location.Y));
+                int[,] mask = new int[w, h];
+
+                while (fillPoints.Count > 0)
+                {
+
+                    System.Drawing.Point p = fillPoints.Pop();
+                    mask[p.X, p.Y] = 1;
+                    temp[3 * p.X + p.Y * stride] = (byte)fillColor.B;
+                    temp[3 * p.X + 1 + p.Y * stride] = (byte)fillColor.G;
+                    temp[3 * p.X + 2 + p.Y * stride] = (byte)fillColor.R;
+                    if (p.X > 0 && (Math.Abs(gray - (int)((grayValues[3 * (p.X - 1) + p.Y * stride] + grayValues[3 * (p.X - 1) + 1 + p.Y * stride] + grayValues[3 * (p.X - 1) + 2 + p.Y * stride]) / 3)) < threshould) && (mask[p.X - 1, p.Y] != 1))
+                    {
+                        temp[3 * (p.X - 1) + p.Y * stride] = (byte)fillColor.B;
+                        temp[3 * (p.X - 1) + 1 + p.Y * stride] = (byte)fillColor.G;
+                        temp[3 * (p.X - 1) + 2 + p.Y * stride] = (byte)fillColor.R;
+                        fillPoints.Push(new System.Drawing.Point(p.X - 1, p.Y));
+                        mask[p.X - 1, p.Y] = 1;
+                    }
+                    if (p.X < w - 1 && (Math.Abs(gray - (int)((grayValues[3 * (p.X + 1) + p.Y * stride] + grayValues[3 * (p.X + 1) + 1 + p.Y * stride] + grayValues[3 * (p.X + 1) + 2 + p.Y * stride]) / 3)) < threshould) && (mask[p.X + 1, p.Y] != 1))
+                    {
+                        temp[3 * (p.X + 1) + p.Y * stride] = (byte)fillColor.B;
+                        temp[3 * (p.X + 1) + 1 + p.Y * stride] = (byte)fillColor.G;
+                        temp[3 * (p.X + 1) + 2 + p.Y * stride] = (byte)fillColor.R;
+                        fillPoints.Push(new System.Drawing.Point(p.X + 1, p.Y));
+                        mask[p.X + 1, p.Y] = 1;
+                    }
+                    if (p.Y > 0 && (Math.Abs(gray - (int)((grayValues[3 * p.X + (p.Y - 1) * stride] + grayValues[3 * p.X + 1 + (p.Y - 1) * stride] + grayValues[3 * p.X + 2 + (p.Y - 1) * stride]) / 3)) < threshould) && (mask[p.X, p.Y - 1] != 1))
+                    {
+                        temp[3 * p.X + (p.Y - 1) * stride] = (byte)fillColor.B;
+                        temp[3 * p.X + 1 + (p.Y - 1) * stride] = (byte)fillColor.G;
+                        temp[3 * p.X + 2 + (p.Y - 1) * stride] = (byte)fillColor.R;
+                        fillPoints.Push(new System.Drawing.Point(p.X, p.Y - 1));
+                        mask[p.X, p.Y - 1] = 1;
+                    }
+                    if (p.Y < h - 1 && (Math.Abs(gray - (int)((grayValues[3 * p.X + (p.Y + 1) * stride] + grayValues[3 * p.X + 1 + (p.Y + 1) * stride] + grayValues[3 * p.X + 2 + (p.Y + 1) * stride]) / 3)) < threshould) && (mask[p.X, p.Y + 1] != 1))
+                    {
+                        temp[3 * p.X + (p.Y + 1) * stride] = (byte)fillColor.B;
+                        temp[3 * p.X + 1 + (p.Y + 1) * stride] = (byte)fillColor.G;
+                        temp[3 * p.X + 2 + (p.Y + 1) * stride] = (byte)fillColor.R;
+                        fillPoints.Push(new System.Drawing.Point(p.X, p.Y + 1));
+                        mask[p.X, p.Y + 1] = 1;
+                    }
+                }
+                fillPoints.Clear();
+
+                System.Runtime.InteropServices.Marshal.Copy(temp, 0, dstptr, bytes);
+                srcbmp.UnlockBits(bmpData);
+                dstbmp.UnlockBits(dstbmpData);
+
+                return dstbmp;
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show(exp.Message);
+                return null;
+            }
+        }
+
+        //private bool FloodFillTOcanny()
+        //{
+
+
+        //    System.IO.MemoryStream ms = new System.IO.MemoryStream();
+        //    BmpBitmapEncoder encoder = new BmpBitmapEncoder();
+        //    encoder.Frames.Add(BitmapFrame.Create((BitmapSource)this.cimage.Source));
+        //    encoder.Save(ms);
+
+        //    Bitmap bp = new Bitmap(ms);
+
+        //    Image<Bgr, Byte> srcimg = new Image<Bgr, Byte>(bp);
+        //    //转成灰度图
+        //    Image<Gray, Byte> grayimg = srcimg.Convert<Gray, Byte>();
+        //    // CvInvoke.BitwiseNot(grayimg, grayimg);
+        //    //Canny 边缘检测
+        //    Image<Gray, Byte> cannyGrayimg = grayimg.Canny((int)numericUD_FloodFill.Value, (int)numericUD_FloodFill.Value);
+        //    Gray bkGrayWhite = new Gray(255);
+        //    Emgu.CV.IOutputArray hierarchy = new Image<Gray, Byte>(srcimg.Width, srcimg.Height, bkGrayWhite);
+        //    Image<Rgb, Byte> imgresult = new Image<Rgb, Byte>(srcimg.Width, srcimg.Height, new Rgb(255, 255, 255));
+
+        //    CvInvoke.FindContours(cannyGrayimg,
+        //                            contours,
+        //                            (Emgu.CV.IOutputArray)hierarchy,
+        //                            Emgu.CV.CvEnum.RetrType.Ccomp,
+        //                            Emgu.CV.CvEnum.ChainApproxMethod.ChainApproxNone//保存为
+        //                             );
+        //    GraphicsPath myGraphicsPath = new GraphicsPath();
+        //    Region myRegion = new Region();
+        //    myGraphicsPath.Reset();
+        //    contours.GetOutputArray().GetSize();
+        //    int areaMax = 0, idx = 0;
+        //    for (int ii = 0; ii < contours.Size; ii++)
+        //    {
+        //        int area = (int)CvInvoke.ContourArea(contours[ii]);
+        //        if (area > areaMax) { areaMax = area; idx = ii; }
+        //        if (area < 1) continue;
+        //        CvInvoke.DrawContours(imgresult, contours.GetInputArray(), ii, new MCvScalar(0, 0, 0), 1, Emgu.CV.CvEnum.LineType.EightConnected, (Emgu.CV.IInputArray)null, 2147483647);
+        //        imageBox1.Image = imgresult;
+        //        try
+        //        {
+        //            myGraphicsPath.AddPolygon(contours[ii].ToArray());
+        //        }
+        //        catch
+        //        {
+        //            //MessageBox.Show(e.Message);
+        //        }
+        //    }
+
+        //    myRegion.MakeEmpty();
+        //    myRegion.Union(myGraphicsPath);
+
+        //    pictureBox1.Refresh();
+        //    System.Drawing.Pen pen = new System.Drawing.Pen(System.Drawing.Color.Red, 1);
+        //    pen.DashStyle = System.Drawing.Drawing2D.DashStyle.Dot;
+        //    Graphics gs = pictureBox1.CreateGraphics();
+        //    gs.DrawPath(pen, myGraphicsPath);
+        //    if (myRegion.IsVisible(lastPoint))
+        //    {
+        //        //gs.DrawPolygon(pen, respts);
+        //    }
+        //    else
+        //    {
+        //        //gs.DrawRectangle(pen, new Rectangle(0,0,pictureBox1.Image.Width, pictureBox1.Image.Height));
+        //    }
+        //    gs.Dispose();
+        //    return true;
+        //}
+
+
+        /// 
+        ///end
 
     }
 }
