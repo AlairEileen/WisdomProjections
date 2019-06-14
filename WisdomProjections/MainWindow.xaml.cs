c264f9e9b0fcf0c9870b26245637c0aebf543408
alair <dwalzixe@163.com>
2019-06-14 17:45:21 +0800

    插入tab工作分区 插入左右两侧工具分区 添加3个分区宽度缩放 加入工作分区内容鼠标拖动和鼠标滚轮缩放
    自定义tabItem控件
    自定义GridSplitter控件

--- WisdomProjections/MainWindow.xaml.cs
+++ WisdomProjections/MainWindow.xaml.cs
@@ -17,6 +17,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+using WisdomProjections.Data_Executor;
+using WisdomProjections.Views;
 using YDPeopleSensor.Net;
 
 namespace WisdomProjections
@@ -26,64 +28,13 @@ namespace WisdomProjections
     /// </summary>
     public partial class MainWindow : Window
     {
+        SensorDataExecutor sde;
         public MainWindow()
         {
             InitializeComponent();
-
-            this.palette = new int[0x1000];
-
-            for (uint d = 0; d < 0x190; d++)
-            {
-                unchecked
-                {
-                    this.palette[d] = (int)0xFF000000;
-                }
-            }
-
-            for (uint d = 0x190; d < 0x400; d++)
-            {
-                uint r = 0xFF - 0xFF * (d - 0x190) / (0x400 - 0x190);
-                uint g = 0;
-                uint b = 0xFF;
-
-                this.palette[d] = (int)(b | (g << 8) | (r << 16) | 0xFF000000);
-            }
-
-            for (uint d = 0x400; d < 0x600; d++)
-            {
-                uint r = 0;
-                uint g = 0xFF * (d - 0x400) / (0x600 - 0x400);
-                uint b = 0xFF;
-
-                this.palette[d] = (int)(b | (g << 8) | (r << 16) | 0xFF000000);
-            }
-
-            for (uint d = 0x600; d < 0x800; d++)
-            {
-                uint r = 0;
-                uint g = 0xFF;
-                uint b = 0xFF - 0xFF * (d - 0x600) / (0x800 - 0x600);
-
-                this.palette[d] = (int)(b | (g << 8) | (r << 16) | 0xFF000000);
-            }
-
-            for (uint d = 0x800; d < 0xC00; d++)
-            {
-                uint r = 0xFF * (d - 0x800) / (0xC00 - 0x800);
-                uint g = 0xFF;
-                uint b = 0;
-
-                this.palette[d] = (int)(b | (g << 8) | (r << 16) | 0xFF000000);
-            }
-
-            for (uint d = 0xC00; d < 0x1000; d++)
-            {
-                uint r = 0xFF;
-                uint g = 0xFF - 0xFF * (d - 0xC00) / (0x1000 - 0xC00);
-                uint b = 0;
-
-                this.palette[d] = (int)(b | (g << 8) | (r << 16) | 0xFF000000);
-            }
+            sde = new SensorDataExecutor(tc_Image);
+            Loaded += sde.Window_Loaded;
+            Closing += sde.Window_Closing;
         }
 
 
@@ -91,290 +42,59 @@ namespace WisdomProjections
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            for (var i = 0; i < MaxPersons * MaxJoints; i++)
-            {
-                var joint = new System.Windows.Shapes.Ellipse();
-                joint.Fill = System.Windows.Media.Brushes.Red;
-                joint.Width = 16.0;
-                joint.Height = 16.0;
-                joint.Visibility = System.Windows.Visibility.Collapsed;
-
-                this.canvas.Children.Add(joint);
-            }
-
-            for (var i = 0; i < MaxPersons * MaxBones; i++)
-            {
-                var bone = new Line();
-                bone.Stroke = System.Windows.Media.Brushes.Blue;
-                bone.StrokeThickness = 6.0;
-                bone.Visibility = System.Windows.Visibility.Collapsed;
-
-                this.canvas.Children.Add(bone);
-            }
-
-            this.sensor = new Sensor();
-
-            if (ErrorCode.Success != this.sensor.Initialize(ColorResolution.VGA, DepthResolution.VGA, true, 0))
-            {
-                MessageBox.Show("Failed to initialize sensor", "PeopleSensorAppWpf");
-                this.Close();
-            }
-
-            if (ErrorCode.Success != this.sensor.Start())
-            {
-                MessageBox.Show("Failed to start sensor", "PeopleSensorAppWpf");
-                this.Close();
-            }
-
-            this.sensor.ColorFrameReady += this.OnColorFrameReady;
-            this.sensor.DepthFrameAndPublishDataReady += this.OnDepthFrameAndPublishDataReady;
+            Button btnGrdSplitter = gsSplitterr.Template.FindName("btnExpend", gsSplitterr) as Button;
+            Button btnGrdSplitter2 = gsSplitterr2.Template.FindName("btnExpend", gsSplitterr2) as Button;
+            if (btnGrdSplitter != null)
+                btnGrdSplitter.Click += new RoutedEventHandler(BtnGrdSplitter_Click);
+            if (btnGrdSplitter2 != null)
+                btnGrdSplitter2.Click += new RoutedEventHandler(BtnGrdSplitter2_Click);
         }
 
+      
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
-            this.sensor.ColorFrameReady -= this.OnColorFrameReady;
-            this.sensor.DepthFrameAndPublishDataReady -= this.OnDepthFrameAndPublishDataReady;
-
-            this.sensor.Stop();
-            this.sensor.Uninitialize();
-            this.sensor.Dispose();
+            
         }
 
-        private void OnColorFrameReady(Sensor sensor, ColorFrame frame, ErrorCode error)
+      
+        //private IOutputArray contours;
+      
+    
+
+
+        GridLength m_WidthCache1, m_WidthCache2;
+        int grid1Index = 0, grid2Index = 4;
+
+        public  void BtnGrdSplitter_Click(object sender, RoutedEventArgs e)
         {
-            if (ErrorCode.Success != error)
-            {
-                return;
-            }
-
-            if (null == this.cframe)
-            {
-                this.cframe = new ColorFrame();
-            }
-
-            lock (this.cframe)
-            {
-                frame.CopyTo(this.cframe);
-            }
-
-            Dispatcher.BeginInvoke(new Action(() =>
-            {
-                lock (this.cframe)
-                {
-                    if (null == this.cbitmap || this.cbitmap.PixelWidth != this.cframe.Width || this.cbitmap.PixelHeight != this.cframe.Height)
-                    {
-                        this.cbitmap = new WriteableBitmap(this.cframe.Width, this.cframe.Height, 96, 96, PixelFormats.Bgra32, null);
-                        this.cimage.Source = this.cbitmap;
-                    }
-
-                    this.cbitmap.WritePixels(new Int32Rect(0, 0, this.cframe.Width, this.cframe.Height), this.cframe.Pixels, this.cframe.Width * 4, 0);
-                }
-            }), DispatcherPriority.Render);
+            GridSplitterClick(ref m_WidthCache1, grid1Index);
         }
-
-        private void OnDepthFrameAndPublishDataReady(Sensor sensor, DepthFrame depthFrame, PublishData publishData, ErrorCode error)
+        private void BtnGrdSplitter2_Click(object sender, RoutedEventArgs e)
         {
-
-
-
-
-            if (ErrorCode.Success != error)
-            {
-                return;
-            }
-            if (null == this.dpixels || this.dpixels.Length != depthFrame.Pixels.Length)
-            {
-                this.dpixels = new int[depthFrame.Pixels.Length];
-            }
-
-
-            ///create by alair end
-            //TestPointCloud(sensor,depthFrame,);
-
-            ///create by alair end
-
-            lock (this.dpixels)
-            {
-                this.dw = depthFrame.Width;
-                this.dh = depthFrame.Height;
-                //Console.Write("深度返回的点");
-                for (var i = 0; i < depthFrame.Pixels.Length; i++)
-                {
-                    var d = depthFrame.Pixels[i];
-
-                    if (d >= 0x1000)
-                    {
-                        unchecked
-                        {
-                            this.dpixels[i] = (int)0xFF000000;
-                        }
-                    }
-                    else
-                    {
-                        this.dpixels[i] = this.palette[d];
-                    }
-                    //Console.Write(d+",");
-                }
-                //Console.WriteLine();
-
-                Dispatcher.BeginInvoke(new Action(() =>
-                {
-                    lock (this.dpixels)
-                    {
-                        if (null == this.dbitmap || this.dbitmap.PixelHeight != this.dw || this.dbitmap.PixelHeight != this.dh)
-                        {
-                            this.dbitmap = new WriteableBitmap(this.dw, this.dh, 96, 96, PixelFormats.Bgra32, null);
-                            this.dimage.Source = this.dbitmap;
-                        }
-
-                        this.dbitmap.WritePixels(new Int32Rect(0, 0, this.dw, this.dh), this.dpixels, this.dw * 4, 0);
-                        //this.dbitmap.WritePixels(new Int32Rect(0, 0, this.dw, this.dh), this.dpixels, this.dbitmap.BackBufferStride, 0);
-                    }
-                }), DispatcherPriority.Render);
-            }
-
-            if (null == this.pubData)
-            {
-                this.pubData = new PublishData();
-            }
-
-            lock (this.pubData)
-            {
-                publishData.CopyTo(this.pubData);
-            }
-
-            Dispatcher.BeginInvoke(new Action(() =>
-            {
-                lock (this.pubData)
-                {
-                    this.DrawSkeletons(this.pubData);
-                }
-            }), DispatcherPriority.Render);
+            GridSplitterClick(ref m_WidthCache2, grid2Index);
         }
 
-        private void TestPointCloud(Sensor sensor)
+      
+
+
+        private void GridSplitterClick(ref GridLength w, int grid1Index)
         {
-           //sensor.ConvertToPointCloud()
-        }
-
-        private void DrawSkeletons(PublishData pub)
-        {
-            int jIndex = 0;
-            int bIndex = MaxPersons * MaxJoints;
-            float scaleX = (float)this.canvas.ActualWidth / pub.UserMask.Width;
-            float scaleY = (float)this.canvas.ActualHeight / pub.UserMask.Height;
-
-            for (var i = 0; i < pub.Skeletons.Length; i++)
+            GridLength temp = grdWorkbench.ColumnDefinitions[grid1Index].Width;
+            GridLength def = new GridLength();
+            if (temp.Equals(def))
             {
-                this.DrawSkeleton(pub.Skeletons[i], ref jIndex, ref bIndex, scaleX, scaleY);
+                //恢复
+                grdWorkbench.ColumnDefinitions[grid1Index].Width = w;
             }
-
-            for (var i = jIndex; i < MaxPersons * MaxJoints; i++)
+            else
             {
-                this.canvas.Children[i].Visibility = System.Windows.Visibility.Collapsed;
-            }
-
-            for (var i = bIndex; i < this.canvas.Children.Count; i++)
-            {
-                this.canvas.Children[i].Visibility = System.Windows.Visibility.Collapsed;
+                //折叠
+                w = grdWorkbench.ColumnDefinitions[grid1Index].Width;
+                grdWorkbench.ColumnDefinitions[grid1Index].Width = def;
             }
         }
 
-        private void DrawSkeleton(Skeleton skeleton, ref int jointIndex, ref int boneIndex, float scaleX, float scaleY)
-        {
-            foreach (var joint in skeleton.Joints)
-            {
-                this.DrawJoint(joint.Position, ref jointIndex, scaleX, scaleY);
-            }
 
-            this.DrawBone(skeleton.Joints[0].Position, skeleton.Joints[1].Position, ref boneIndex, scaleX, scaleY);
-            this.DrawBone(skeleton.Joints[1].Position, skeleton.Joints[2].Position, ref boneIndex, scaleX, scaleY);
-            this.DrawBone(skeleton.Joints[1].Position, skeleton.Joints[3].Position, ref boneIndex, scaleX, scaleY);
-            this.DrawBone(skeleton.Joints[2].Position, skeleton.Joints[4].Position, ref boneIndex, scaleX, scaleY);
-            this.DrawBone(skeleton.Joints[3].Position, skeleton.Joints[5].Position, ref boneIndex, scaleX, scaleY);
-            this.DrawBone(skeleton.Joints[4].Position, skeleton.Joints[6].Position, ref boneIndex, scaleX, scaleY);
-            this.DrawBone(skeleton.Joints[5].Position, skeleton.Joints[7].Position, ref boneIndex, scaleX, scaleY);
-            this.DrawBone(skeleton.Joints[2].Position, skeleton.Joints[11].Position, ref boneIndex, scaleX, scaleY);
-            this.DrawBone(skeleton.Joints[3].Position, skeleton.Joints[10].Position, ref boneIndex, scaleX, scaleY);
-            this.DrawBone(skeleton.Joints[10].Position, skeleton.Joints[12].Position, ref boneIndex, scaleX, scaleY);
-            this.DrawBone(skeleton.Joints[11].Position, skeleton.Joints[13].Position, ref boneIndex, scaleX, scaleY);
-            this.DrawBone(skeleton.Joints[12].Position, skeleton.Joints[14].Position, ref boneIndex, scaleX, scaleY);
-            this.DrawBone(skeleton.Joints[13].Position, skeleton.Joints[15].Position, ref boneIndex, scaleX, scaleY);
-            this.DrawBone(skeleton.Joints[10].Position, skeleton.Joints[11].Position, ref boneIndex, scaleX, scaleY);
-        }
-
-        private void DrawJoint(Float4 position, ref int index, float scaleX, float scaleY)
-        {
-            if (float.IsNaN(position.X) || float.IsNaN(position.Y) || float.IsNaN(position.Z))
-            {
-                return;
-            }
-
-            if (0 == position.Z)
-            {
-                return;
-            }
-
-            var p = this.Project(position, scaleX, scaleY);
-            var e = (System.Windows.Shapes.Ellipse)this.canvas.Children[index++];
-
-            Canvas.SetLeft(e, p.X - e.Width * 0.5);
-            Canvas.SetTop(e, p.Y - e.Width * 0.5);
-            e.Visibility = System.Windows.Visibility.Visible;
-        }
-
-        private void DrawBone(Float4 p0, Float4 p1, ref int index, float scaleX, float scaleY)
-        {
-            if (float.IsNaN(p0.X) || float.IsNaN(p0.Y) || float.IsNaN(p0.Z) ||
-                float.IsNaN(p1.X) || float.IsNaN(p1.Y) || float.IsNaN(p1.Z))
-            {
-                return;
-            }
-
-            if (0.0f == p0.Z || 0.0f == p1.Z)
-            {
-                // TODO: investigate why z = 0
-                return;
-            }
-
-            var pt0 = this.Project(p0, scaleX, scaleY);
-            var pt1 = this.Project(p1, scaleX, scaleY);
-
-            var l = (Line)this.canvas.Children[index++];
-            l.X1 = pt0.X;
-            l.Y1 = pt0.Y;
-            l.X2 = pt1.X;
-            l.Y2 = pt1.Y;
-            l.Visibility = System.Windows.Visibility.Visible;
-        }
-
-        private System.Windows.Point Project(Float4 position, float scaleX, float scaleY)
-        {
-            Float2 point2D = new Float2();
-            this.sensor.DepthSpacePointToScreen(position, ref point2D);
-
-            var p =new System.Windows.Point();
-            p.X = point2D.X * scaleX;
-            p.Y = point2D.Y * scaleY;
-
-            return p;
-        }
-
-        private Sensor sensor;
-
-        private ColorFrame cframe;
-        private WriteableBitmap cbitmap;
-
-        private int dw, dh;
-        private int[] dpixels;
-        private int[] palette;
-        private WriteableBitmap dbitmap;
-
-        private PublishData pubData;
-        private IOutputArray contours;
-        private const int MaxPersons = 6;
-        private const int MaxJoints = 17;
-        private const int MaxBones = 14;
 
         ///create by alair
         ///
@@ -462,78 +182,6 @@ namespace WisdomProjections
             }
         }
 
-        //private bool FloodFillTOcanny()
-        //{
-
-
-        //    System.IO.MemoryStream ms = new System.IO.MemoryStream();
-        //    BmpBitmapEncoder encoder = new BmpBitmapEncoder();
-        //    encoder.Frames.Add(BitmapFrame.Create((BitmapSource)this.cimage.Source));
-        //    encoder.Save(ms);
-
-        //    Bitmap bp = new Bitmap(ms);
-
-        //    Image<Bgr, Byte> srcimg = new Image<Bgr, Byte>(bp);
-        //    //转成灰度图
-        //    Image<Gray, Byte> grayimg = srcimg.Convert<Gray, Byte>();
-        //    // CvInvoke.BitwiseNot(grayimg, grayimg);
-        //    //Canny 边缘检测
-        //    Image<Gray, Byte> cannyGrayimg = grayimg.Canny((int)numericUD_FloodFill.Value, (int)numericUD_FloodFill.Value);
-        //    Gray bkGrayWhite = new Gray(255);
-        //    Emgu.CV.IOutputArray hierarchy = new Image<Gray, Byte>(srcimg.Width, srcimg.Height, bkGrayWhite);
-        //    Image<Rgb, Byte> imgresult = new Image<Rgb, Byte>(srcimg.Width, srcimg.Height, new Rgb(255, 255, 255));
-
-        //    CvInvoke.FindContours(cannyGrayimg,
-        //                            contours,
-        //                            (Emgu.CV.IOutputArray)hierarchy,
-        //                            Emgu.CV.CvEnum.RetrType.Ccomp,
-        //                            Emgu.CV.CvEnum.ChainApproxMethod.ChainApproxNone//保存为
-        //                             );
-        //    GraphicsPath myGraphicsPath = new GraphicsPath();
-        //    Region myRegion = new Region();
-        //    myGraphicsPath.Reset();
-        //    contours.GetOutputArray().GetSize();
-        //    int areaMax = 0, idx = 0;
-        //    for (int ii = 0; ii < contours.Size; ii++)
-        //    {
-        //        int area = (int)CvInvoke.ContourArea(contours[ii]);
-        //        if (area > areaMax) { areaMax = area; idx = ii; }
-        //        if (area < 1) continue;
-        //        CvInvoke.DrawContours(imgresult, contours.GetInputArray(), ii, new MCvScalar(0, 0, 0), 1, Emgu.CV.CvEnum.LineType.EightConnected, (Emgu.CV.IInputArray)null, 2147483647);
-        //        imageBox1.Image = imgresult;
-        //        try
-        //        {
-        //            myGraphicsPath.AddPolygon(contours[ii].ToArray());
-        //        }
-        //        catch
-        //        {
-        //            //MessageBox.Show(e.Message);
-        //        }
-        //    }
-
-        //    myRegion.MakeEmpty();
-        //    myRegion.Union(myGraphicsPath);
-
-        //    pictureBox1.Refresh();
-        //    System.Drawing.Pen pen = new System.Drawing.Pen(System.Drawing.Color.Red, 1);
-        //    pen.DashStyle = System.Drawing.Drawing2D.DashStyle.Dot;
-        //    Graphics gs = pictureBox1.CreateGraphics();
-        //    gs.DrawPath(pen, myGraphicsPath);
-        //    if (myRegion.IsVisible(lastPoint))
-        //    {
-        //        //gs.DrawPolygon(pen, respts);
-        //    }
-        //    else
-        //    {
-        //        //gs.DrawRectangle(pen, new Rectangle(0,0,pictureBox1.Image.Width, pictureBox1.Image.Height));
-        //    }
-        //    gs.Dispose();
-        //    return true;
-        //}
-
-
-        /// 
-        ///end
-
+       
     }
 }
---
+++ WisdomProjections/Views/ImageTabItem.xaml.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WisdomProjections.Views
+{
+    /// <summary>
+    /// ImageTabItem.xaml 的交互逻辑
+    /// </summary>
+    public partial class ImageTabItem : TabItem
+    {
+        private TabControl tabControl;
+
+        public ImageTabItem(string header,bool canClose, TabControl tabControl)
+        {
+            InitializeComponent();
+            this.tabControl = tabControl;
+            if (header.Length > 8)
+            {
+                var ex = header.Substring(header.LastIndexOf("."));
+                var c = 8 - ex.Length - 2;
+                header = "**" + header.Substring(header.LastIndexOf(".") - c, c) + ex;
+            }
+            this.tHeader.Content = header;
+            if (!canClose)
+            {
+                this.tClose.Visibility = Visibility.Collapsed;
+            }
+            tabControl.Items.Add(this);
+
+        }
+
+        public ImageSource GetImageSource()
+        {
+
+            return img.Source;
+        }
+        public void SetImageSource(ImageSource imageSource)
+        {
+            App.Current.Dispatcher.Invoke(() =>
+            {
+                img.Source = imageSource;
+            });
+        }
+        private void img_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            var img = sender as ContentControl;
+            if (img == null)
+            {
+                return;
+            }
+            var point = e.GetPosition(img);
+            var group = IMG.FindResource("Imageview") as TransformGroup;
+            var delta = e.Delta * 0.001;
+            DowheelZoom(group, point, delta);
+        }
+        private void DowheelZoom(TransformGroup group, Point point, double delta)
+        {
+            var pointToContent = group.Inverse.Transform(point);
+            var transform = group.Children[0] as ScaleTransform;
+            if (transform.ScaleX + delta < 0.1) return;
+            transform.ScaleX += delta;
+            transform.ScaleY += delta;
+            var transform1 = group.Children[1] as TranslateTransform;
+            transform1.X = -1 * ((pointToContent.X * transform.ScaleX) - point.X);
+            transform1.Y = -1 * ((pointToContent.Y * transform.ScaleY) - point.Y);
+        }
+        private void TClose_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            tabControl.Items.Remove(this);
+        }
+
+        private void Img_MouseMove(object sender, MouseEventArgs e)
+        {
+            var img = sender as ContentControl;
+            if (img == null)
+            {
+                return;
+            }
+            if (mouseDown)
+            {
+                Domousemove(img, e);
+            }
+        }
+        private void Domousemove(ContentControl img, MouseEventArgs e)
+        {
+            if (e.LeftButton != MouseButtonState.Pressed)
+            {
+                return;
+            }
+            var group = IMG.FindResource("Imageview") as TransformGroup;
+            var transform = group.Children[1] as TranslateTransform;
+            var position = e.GetPosition(img);
+            transform.X -= mouseXY.X - position.X;
+            transform.Y -= mouseXY.Y - position.Y;
+            mouseXY = position;
+        }
+
+        private bool mouseDown;
+        private Point mouseXY;
+
+
+        private void Img_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var img = sender as ContentControl;
+            if (img == null)
+            {
+                return;
+            }
+            img.CaptureMouse();
+            mouseDown = true;
+            mouseXY = e.GetPosition(img);
+        }
+
+        private void Img_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            var img = sender as ContentControl;
+            if (img == null)
+            {
+                return;
+            }
+            img.ReleaseMouseCapture();
+            mouseDown = false;
+
+        }
+    }
+}
